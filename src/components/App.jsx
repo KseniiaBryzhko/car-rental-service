6ea2696335a837ec88c1cb6f427831f0da7c8380
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-09-11 11:16:59 +0300

    pages added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
+import Favorites from 'pages/Favorites';
+import NotFound from 'pages/NotFound';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/catalog">Catalog</StyledLink>
+        <StyledLink to="/favorites">Favorites</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
